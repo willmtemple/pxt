7afe9c29700204dee02da45ba6e58579e17701e0
Peli de Halleux <jhalleux@microsoft.com>
2017-05-01 15:43:51 -0700

    updated junior toolbox

--- webapp/src/juniortoolbox.ts
+++ webapp/src/juniortoolbox.ts
@@ -1,5 +1,5 @@
 export default new DOMParser().parseFromString(`<xml id="blocklyToolboxDefinition" style="display: none">
-            <block type="controls_repeat_ext" colour="#1DDF1D">
+            <block type="controls_repeat_ext" colour="#1DDF1D" gap="8">
                 <value name="TIMES">
                     <shadow type="math_number">
                         <field name="NUM">4</field>
